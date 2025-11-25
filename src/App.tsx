a2324196152d80d80dc13a2aaba43b5d96633f37
Chukwuebuka Onah <revelationjay02@gmail.com>
2025-11-25 09:33:17 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css'
-import BusinessModelCanvas from './components/BusinessModelCanvas'
-import FlowComponent from './components/FlowComponent'
 import NewBusinessModelCanvas from './components/NewBusinessModelCanvas'
 
 function App() {
